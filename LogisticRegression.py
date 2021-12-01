a2116ab87816ad431cebf215e1ebf1078247bcc5
hspinos <hspinos@gmail.com>
2021-11-30 21:46:54 -0500

    Initial Commit
    Create a script for each model that we plan to use

---
+++ LogisticRegression.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
